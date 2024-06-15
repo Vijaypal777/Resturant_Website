8b3fa056daa4fb091966fca399c371d4b5b13a43
vijaypal <vpsaini777@gmai.com>
2024-06-15 19:43:34 +0530

    my first commit reactwebsite

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import MenuPage from './pages/MenuPage'
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import About from './pages/About';
+import Contact from './pages/Contact';
+import PageNotFound from './pages/PageNotFound';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<HomePage/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/menu' element={<MenuPage/>}/>
+      <Route path='*' element={<PageNotFound/>}/>
+    </Routes>
+    </BrowserRouter>
+    </div >
   );
 }
 
